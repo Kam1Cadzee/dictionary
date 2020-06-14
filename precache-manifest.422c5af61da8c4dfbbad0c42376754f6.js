3c25cfc585de530024e6e6fd8f0d064eefeb84f5
Vadim Vereketa <kam1cadzee@gmail.com>
2020-06-14 16:49:36 +0300

    Updates

---
+++ precache-manifest.422c5af61da8c4dfbbad0c42376754f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87703584d505940217160c4a423cccc9",
+    "url": "/dictionary/index.html"
+  },
+  {
+    "revision": "93f18fee0333393292ae",
+    "url": "/dictionary/static/css/2.a2026422.chunk.css"
+  },
+  {
+    "revision": "54848194b51ea501a862",
+    "url": "/dictionary/static/css/main.e2a0fdce.chunk.css"
+  },
+  {
+    "revision": "93f18fee0333393292ae",
+    "url": "/dictionary/static/js/2.95d83efe.chunk.js"
+  },
+  {
+    "revision": "b5901214077ea32a7534a1d4eff7c84a",
+    "url": "/dictionary/static/js/2.95d83efe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54848194b51ea501a862",
+    "url": "/dictionary/static/js/main.f28eb0e4.chunk.js"
+  },
+  {
+    "revision": "ab2ae2e6bf8d4499a3a3",
+    "url": "/dictionary/static/js/runtime-main.4835b875.js"
+  }
+]);
